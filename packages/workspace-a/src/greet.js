767874fd3f59677b76c0ed05b930c37c23bd87f0
Florestan Lefevre <florestanlefevre@gmail.com>
2020-03-29 21:45:36 -0700

    Add babel-loader so that webpack transforms our code before creating our output bundle

---
+++ packages/workspace-a/src/greet.js
@@ -0,0 +1,5 @@
+const greeting = (name) => {
+  return `Hello ${name}`;
+};
+
+export default greeting;
--- packages/workspace-a/src/index.js
+++ packages/workspace-a/src/index.js
@@ -1,1 +1,3 @@
-console.log('Hello World');
+import greeting from './greet';
+
+console.log(greeting('Flo'));
