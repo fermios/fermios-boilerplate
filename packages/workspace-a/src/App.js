894c878e7f07f534c07ffe7fb4fb1a13a5975498
Florestan Lefevre <florestanlefevre@gmail.com>
2020-04-01 20:52:56 -0700

    Add @babel/plugin-proposal-class-properties to read js ES6 features

--- packages/workspace-a/src/App.js
+++ packages/workspace-a/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    test: 0
+  };
+
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button onClick={() => this.setState({ test: this.state.test + 1})}>
+          {this.state.test}
+        </button>
+      </div>
+    );
   }
 }
 
