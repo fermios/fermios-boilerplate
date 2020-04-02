ca387d0fc9a1f68911de4bc1ff097e6afb9424c5
Florestan Lefevre <florestanlefevre@gmail.com>
2020-04-01 23:06:50 -0700

    Install Jest and test it

--- packages/workspace-a/src/index.js
+++ packages/workspace-a/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@babel/polyfill'
 import App from './App';
-import './styles.css';
 
 ReactDOM.render(<App/>, document.getElementById('app'));
