730e993d5f094aec432a3bddbde0262deeda50d6
Florestan Lefevre <florestanlefevre@gmail.com>
2020-04-07 22:10:08 -0700

    Add ErrorBoundary

--- packages/workspace-a/src/index.js
+++ packages/workspace-a/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@babel/polyfill';
 import App from './App';
+import DefaultErrorBoundary from './DefaultErrorBoundary';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
   </React.StrictMode>,
   document.getElementById('app')
 );
